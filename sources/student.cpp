dd253f0257ed4bbcbbbbef56a6d69af5733d793f
NickBBn <nickgeo.winner@gmail.com>
2020-10-05 21:58:19 +0300

    Fixing brackets

--- sources/student.cpp
+++ sources/student.cpp
@@ -60,24 +60,31 @@ std::string Student::group_string() const
 {
   if (_group.type() == typeid(int))
     return std::to_string(std::any_cast<int>(_group));
-  else if (_group.type() == typeid(float)){
+  else if (_group.type() == typeid(float))
+  {
     std::string tmp = std::to_string(std::any_cast<float>(_group));
     while (tmp.at(tmp.size()-1) == '0')
       tmp = tmp.substr(0, tmp.size()-1);
     return tmp;
+  } else {
+    return std::any_cast<std::string>(_group);
   }
-  else return std::any_cast<std::string>(_group);
 }
 std::string Student::avg_string() const
 {
   if (_avg.type() == typeid(int))
     return std::to_string(std::any_cast<int>(_avg));
-  else if (_avg.type() == typeid(float_t)){
+  else if (_avg.type() == typeid(float_t))
+  {
     std::string tmp = std::to_string(std::any_cast<float>(_avg));
     while (tmp.at(tmp.size()-1) == '0')
       tmp = tmp.substr(0, tmp.size()-1);
     return tmp;
-  } else return std::any_cast<std::string>(_avg);
+  }
+  else
+  {
+    return std::any_cast<std::string>(_avg);
+  }
 }
 std::string Student::debt_string() const
 {
@@ -89,7 +96,10 @@ std::string Student::debt_string() const
     size = std::any_cast<std::vector<std::string>>(_debt).size();
   //  return "this debt is vector";
     return std::to_string(size);
+  } else {
+    if (_debt.type() == typeid(std::nullptr_t)) return "null";
+    else return std::any_cast<std::string>(_debt);
   }
-  else if (_debt.type() == typeid(std::nullptr_t)) return "null";
-  else return std::any_cast<std::string>(_debt);
+
+
 }
